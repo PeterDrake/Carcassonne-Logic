c6bdbfb726fdb78c939fa3370e089986a8608e61
Rudy Reeves <rudyreeves@lclark.edu>
2014-01-30 15:57:39 -0800

    Added inside() method to Game

--- src/Game.java
+++ src/Game.java
@@ -1,16 +1,25 @@
-
 public class Game {
     private GUI gui;
     private Board board;
     private Scorer scorer;
     private Tile currentTile;
-    private static int NUM_TILES = 72;
-    
+    private static final int NUM_TILES = 72;
+    private static final int BOARD_SIZE = Game.NUM_TILES * Tile.SIZE;
+
     public Game() {
         gui = new GUI(this);
     }
-    
+
     public void mouseClicked(int x, int y) {
+        if (inside(x, y)) {
+            // place a tile on the board
+        }
+        
+        // else if we are picking a tile from the pile, we set currentTile
         System.out.println("Mouse clicked: " + x + ", " + y);
+    }
+
+    public boolean inside(int x, int y) {
+        return x < BOARD_SIZE && y < BOARD_SIZE;
     }
 }
--- src/Tile.java
+++ src/Tile.java
@@ -1,5 +1,5 @@
 
 public class Tile {
     public static enum Zone {ROAD, GRASS, CITY, CLOISTER, ROAD_ENDPOINT};
-    public static int SIZE = 90;
+    public static final int SIZE = 90;
 }
