67ad7d0e98cd8490122f93cce5310122cd9d22b7
Rudy Reeves <rudyreeves@lclark.edu>
2014-01-30 14:46:30 -0800

    Added GUI window

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,18 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
 
 public class GUI {
+    private static final int DEFAULT_WIDTH = 800;
+    private static final int DEFAULT_HEIGHT = 600;
 
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new JFrame();
+                frame.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+    }
 }
