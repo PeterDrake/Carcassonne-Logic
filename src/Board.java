dde58f04cd7c7d3c389a59bdc28f77c77ce2acc8
Spencer Wuthmann <swuthmann@lclark.edu>
2014-01-30 15:43:36 -0800

    nothing was in the file before, we made an incomplete/bare meeple placement method for the class

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,24 @@
 
 public class Board {
+	
+	// takes in an x & y cordinate from a mouseclick, places meeple where clicked
+	public int placeMeeple(int[] location, Player player, int type) {
+		switch(type) {
+		case 0: 
+			System.out.println("farmer");
+		case 1:
+			System.out.println("thief");
+		case 3:
+			System.out.println("knight");
+		case 4:
+			System.out.println("monk");
+		}
+//		draw meeple using information from specific case and coordinates taken in
+		
+//		this should be fixed
+		player.meeple--;
+		
+		return -1;
+	}
 
 }
