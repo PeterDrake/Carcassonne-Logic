72441e6c78116c420b0d974c68006a4ae156dded
Kekai Clarke <kclarke@lclark.edu>
2014-02-04 14:25:28 -0800

    Completed meeple placement method

--- src/Board.java
+++ src/Board.java
@@ -1,24 +1,50 @@
+import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
 
-public class Board {
-	
-	// takes in an x & y cordinate from a mouseclick, places meeple where clicked
+public class Board extends JFrame {
+	Image meepleImage;
+	int x, y;
+
+	// takes in an x & y cordinate from a mouseclick, places meeple where
+	// clicked
 	public int placeMeeple(int[] location, Player player, int type) {
-		switch(type) {
-		case 0: 
-			System.out.println("farmer");
-			
-		case 1:
-			System.out.println("thief");
-		case 3:
-			System.out.println("knight");
-		case 4:
-			System.out.println("monk");
+		String meepleType;
+		String color = "translucent";
+		x = location[0];
+		y = location[1];
+		
+		if (player != null) {
+			color = player.color;
 		}
-//		draw meeple using information from specific case and coordinates taken in
-		
-//		this should be fixed
-		player.meeple--;
-		
+		switch (type) {
+		case 0:
+			meepleType = color + "Farmer.png";
+		case 1:
+			meepleType = color + "Thief.png";
+		case 3:
+			meepleType = color + "Knight.png";
+		case 4:
+			meepleType = color + "Monk.png";
+		}
+		// draw meeple using information from specific case and coordinates
+		// taken in
+		meepleImage = new ImageIcon(meepleType).getImage();
+
+		add(new JComponent() {
+			public void paintComponent(Graphics g) {
+				if (meepleImage != null) {
+					g.drawImage(meepleImage, x, y, null);
+				}
+			}
+		});
+
+		// this should be fixed
+		if (player != null) {
+			player.meeple--;
+		}
+
 		return -1;
 	}
 
