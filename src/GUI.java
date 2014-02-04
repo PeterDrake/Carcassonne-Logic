778bba37a74aabeb7050c6169ded94e8baa3dccb
James Fischer <jamesfischer@lclark.edu>
2014-02-04 13:10:42 -0800

    can display tile

--- src/GUI.java
+++ src/GUI.java
@@ -1,18 +1,26 @@
+
+
 import java.awt.*;
 import javax.swing.*;
 
-public class GUI {
-	private static final int DEFAULT_WIDTH = 800;
-	private static final int DEFAULT_HEIGHT = 600;
-
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				JFrame frame = new JFrame();
-				frame.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setVisible(true);
-			}
-		});
-	}
+/**
+ * This program demonstrates various image processing operations.
+ * @version 1.03 2007-08-16
+ * @author Cay Horstmann
+ */
+public class GUI
+{
+   public static void main(String[] args)
+   {
+      EventQueue.invokeLater(new Runnable()
+         {
+            public void run()
+            {
+               JFrame frame = new TileFrame();
+               frame.setTitle("Carcassonne");
+               frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+               frame.setVisible(true);
+            }
+         });
+   }
 }
---
+++ src/TileFrame.java
@@ -0,0 +1,61 @@
+
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.image.*;
+import javax.swing.*;
+
+/**
+ * This frame has a menu to load an image and to specify various
+ * transformations, and a component to show the resulting image.
+ */
+public class TileFrame extends JFrame {
+	private static final int DEFAULT_WIDTH = 400;
+	private static final int DEFAULT_HEIGHT = 400;
+	private String tileName = "CarcassonneTile.jpg";
+	private int numRotation = 2;
+
+	private BufferedImage image;
+
+	public TileFrame() {
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+
+		Image img = new ImageIcon(tileName).getImage();
+		image = new BufferedImage(img.getWidth(null), img.getHeight(null),
+				BufferedImage.TYPE_INT_RGB);
+		image.getGraphics().drawImage(img, 0, 0, null);
+
+		for (int i = 0; i < numRotation; i++) {
+			if (image == null)
+				return;
+			AffineTransform transform = AffineTransform.getRotateInstance(
+					Math.toRadians(90), image.getWidth() / 2,
+					image.getHeight() / 2);
+			AffineTransformOp op = new AffineTransformOp(transform,
+					AffineTransformOp.TYPE_BICUBIC);
+			filter(op);
+		}
+		
+		add(new JComponent() {
+			public void paintComponent(Graphics g) {
+				if (image != null)
+					g.drawImage(image,
+							DEFAULT_WIDTH / 2 - image.getWidth() / 2,
+							DEFAULT_HEIGHT / 2 - image.getHeight() / 2, null);
+			}
+		});
+	}
+
+	/**
+	 * Apply a filter and repaint.
+	 * 
+	 * @param op
+	 *            the image operation to apply
+	 */
+	private void filter(BufferedImageOp op) {
+		if (image == null)
+			return;
+		image = op.filter(image, null);
+		repaint();
+	}
+
+}
