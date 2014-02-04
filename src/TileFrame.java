abb5f2e850f1fe9d6755efa8a2244b2cc36a01ff
Kaleb Ganz <kjganz@gmail.com>
2014-02-04 13:59:57 -0800

    more compatible

--- src/TileFrame.java
+++ src/TileFrame.java
@@ -11,32 +11,24 @@ import javax.swing.*;
 public class TileFrame extends JFrame {
 	private static final int DEFAULT_WIDTH = 90;
 	private static final int DEFAULT_HEIGHT = 90;
-	private String tileName;
-	private int numRotation;
 
 	private BufferedImage image;
 
 	public TileFrame(Tile tile, int numRotation) {
 		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 		
-		tileName = tile.getImage();
-		this.numRotation = numRotation;
-
-		Image img = new ImageIcon(tileName).getImage();
+		Image img = new ImageIcon(tile.getImage()).getImage();
 		image = new BufferedImage(img.getWidth(null), img.getHeight(null),
 				BufferedImage.TYPE_INT_RGB);
 		image.getGraphics().drawImage(img, 0, 0, null);
 
-		for (int i = 0; i < numRotation; i++) {
-			if (image == null)
-				return;
-			AffineTransform transform = AffineTransform.getRotateInstance(
-					Math.toRadians(90), image.getWidth() / 2,
-					image.getHeight() / 2);
-			AffineTransformOp op = new AffineTransformOp(transform,
-					AffineTransformOp.TYPE_BICUBIC);
-			filter(op);
-		}
+		if (image == null) return;
+		AffineTransform transform = AffineTransform.getRotateInstance(
+				Math.toRadians(90*numRotation), image.getWidth() / 2,
+				image.getHeight() / 2);
+		AffineTransformOp op = new AffineTransformOp(transform,
+				AffineTransformOp.TYPE_BICUBIC);
+		filter(op);
 		
 		add(new JComponent() {
 			public void paintComponent(Graphics g) {
