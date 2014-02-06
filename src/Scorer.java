e748ce8811a48121906b049533e1f2f015ec3bf0
Spencer Wuthmann <swuthmann@lclark.edu>
2014-02-06 13:48:02 -0800

    bare bones of a road completion method

--- src/Scorer.java
+++ src/Scorer.java
@@ -1,4 +1,13 @@
-
 public class Scorer {
-
+	public boolean roadComplete(Tile tile) {
+		if (!tile.containsRoad() || !tile.containsRoadEnd()) {
+			return false;
+		
+		for (Tile tile : neighbors) {
+			if (tile.isConnected(tile.next)) {
+				
+			}
+		}
+		}	
+	}
 }
