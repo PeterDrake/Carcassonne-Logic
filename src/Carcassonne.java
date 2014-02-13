b5ac7e9da983e36220db655f2b4324f16aab2336
Kaleb Ganz <kganz@lclark.edu>
2014-02-13 15:48:58 -0800

    made sure start tile also prints, good luck reading

--- src/Carcassonne.java
+++ src/Carcassonne.java
@@ -2,7 +2,8 @@ public class Carcassonne {
 
 	public static void main(String[] args) {
 		Deck deck = new Deck();
-		System.out.println(deck.toString());
+		Tile tile = new Tile(24);
+		System.out.println(tile.toString() + "\n" + deck.toString());
 		
 		System.out.println("0 = grass\n1 = road\n2 = road end point\n"
 				+ "3 = city\n4 = cloister\n5 = city crest\n");
